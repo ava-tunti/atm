32154ca517c4ba09caf9926f391e7115afbd85df
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-03-18 14:52:02 -0700

    Update solutionStage3.jsx

--- solutionStage3.jsx
+++ solutionStage3.jsx
@@ -14,6 +14,7 @@ const Account = () => {
   let deposit = 0; // state of this transaction
   const [totalState, setTotalState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
+  const [selectedOption, setSelectedOption] = React.useState(""); // State for selected option
 
   let status = `Account Balance $ ${totalState} `;
   console.log(`Account Rendered with isDeposit: ${isDeposit}`);
@@ -31,17 +32,25 @@ const Account = () => {
       alert("Error. Not enough funds. Try again.");
       event.preventDefault();
     }
-    
+  };
+
+  const handleSelectChange = event => {
+    const selectedValue = event.target.value;
+    setSelectedOption(selectedValue);
+    if (selectedValue === "Deposit") {
+      setIsDeposit(true);
+    } else if (selectedValue === "Cash Back") {
+      setIsDeposit(false);
+    } else {
+      setSelectedOption("");
+      setIsDeposit(true); // Setting default mode to Deposit when nothing is selected
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2 id="total">{status}</h2>
-      <button onClick={() => setIsDeposit(true)}>Deposit</button>
-      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
-      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
-    </form>
-  );
-};
-// ========================================
-ReactDOM.render(<Account />, document.getElementById("root"));
+      <label>Select an action below to continue</label>
+      <select onChange={handleSelectChange} value={selectedOption} name="mode" id="mode-select">
+        <option value="">Select Option</option>
+        <option value="
